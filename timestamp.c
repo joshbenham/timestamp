b8d7edc96f7eecdaccbe1c2a53fbe13e1a5cce3e
Josh Benham <joshbenham@gmail.com>
2024-01-18 16:39:22 +1100

    Simplified

--- timestamp.c
+++ timestamp.c
@@ -2,14 +2,10 @@
 #include <time.h>
 
 int main(int argc, char** argv) {
+    time_t timestamp = time(NULL);
 
-	if (argc == 1) {
-		time_t timestamp = time(NULL);
+    printf("Current time: %s", ctime(&timestamp));
+    printf("Current unix time: %d", (int)timestamp);
 
-		printf("Current time: %s", ctime(&timestamp));
-		printf("Current unix time: %d", (int)timestamp);
-	
-		return 0;
-	}
-
+    return 0;
 }
